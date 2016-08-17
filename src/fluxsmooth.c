40d6ebe1a618aa8d2780edf40cc002a53cd0ce48
cantabile <cantabile.desu@gmail.com>
2016-08-17 17:57:38 +0300

    Remove some duplicated code

--- src/fluxsmooth.c
+++ src/fluxsmooth.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -15,7 +16,8 @@ enum {
     SpatioTemporalFlux
 };
 
-typedef struct {
+
+typedef struct FluxSmoothData {
     VSNodeRef *node;
     const VSVideoInfo *vi;
 
@@ -23,20 +25,20 @@ typedef struct {
     int spatial_threshold;
     int process[3];
 
-    void (*temporal_function)(const uint8_t *, const uint8_t *, const uint8_t *, uint8_t *, int, int, int, int);
-    void (*spatiotemporal_function)(const uint8_t *, const uint8_t *, const uint8_t *, uint8_t *, int, int, int, int, int);
+    void (*flux_function)(const uint8_t *, const uint8_t *, const uint8_t *, uint8_t *, int, int, int, int, int);
 } FluxSmoothData;
 
 
 #if defined(FLUXSMOOTH_X86)
-static void fluxsmooth_temporal_uint8_sse2(const uint8_t *srcpp, const uint8_t *srccp, const uint8_t *srcnp, uint8_t *dstp, int width, int height, int stride, int threshold) {
+static void fluxsmooth_temporal_uint8_sse2(const uint8_t *srcpp, const uint8_t *srccp, const uint8_t *srcnp, uint8_t *dstp, int width, int height, int stride, int temporal_threshold, int spatial_threshold) {
     (void)width;
+    (void)spatial_threshold;
 
 #define zeroes _mm_setzero_si128()
 
     const __m128i bytes_sign_bit = _mm_set1_epi8(128);
 
-    const __m128i bytes_threshold = _mm_add_epi8(_mm_set1_epi8(threshold), bytes_sign_bit);
+    const __m128i bytes_threshold = _mm_add_epi8(_mm_set1_epi8(temporal_threshold), bytes_sign_bit);
 
     const __m128i words_multiplier3 = _mm_set1_epi16(10923);
 
@@ -128,12 +130,13 @@ static void fluxsmooth_temporal_uint8_sse2(const uint8_t *srcpp, const uint8_t *
 }
 
 
-static void fluxsmooth_temporal_uint16_sse2(const uint8_t *srcpp, const uint8_t *srccp, const uint8_t *srcnp, uint8_t *dstp, int width, int height, int stride, int threshold) {
+static void fluxsmooth_temporal_uint16_sse2(const uint8_t *srcpp, const uint8_t *srccp, const uint8_t *srcnp, uint8_t *dstp, int width, int height, int stride, int temporal_threshold, int spatial_threshold) {
     (void)width;
+    (void)spatial_threshold;
 
     __m128i words_sign_bit = _mm_set1_epi16(0x8000);
 
-    __m128i words_threshold = _mm_add_epi16(_mm_set1_epi16(threshold), words_sign_bit);
+    __m128i words_threshold = _mm_add_epi16(_mm_set1_epi16(temporal_threshold), words_sign_bit);
 
     __m128 floats_0_5 = _mm_set1_ps(0.5f);
 
@@ -221,7 +224,9 @@ static void fluxsmooth_temporal_uint16_sse2(const uint8_t *srcpp, const uint8_t 
 #else // FLUXSMOOTH_X86
 
 
-static void fluxsmooth_temporal_uint8_c(const uint8_t *srcpp, const uint8_t *srccp, const uint8_t *srcnp, uint8_t *dstp, int width, int height, int stride, int threshold) {
+static void fluxsmooth_temporal_uint8_c(const uint8_t *srcpp, const uint8_t *srccp, const uint8_t *srcnp, uint8_t *dstp, int width, int height, int stride, int temporal_threshold, int spatial_threshold) {
+    (void)spatial_threshold;
+
     int x, y;
     int16_t magic_numbers[] = { 0, 32767, 16384, 10923 };
     /* Calculated thusly:
@@ -243,12 +248,12 @@ static void fluxsmooth_temporal_uint8_c(const uint8_t *srcpp, const uint8_t *src
                 int sum = curr;
                 int count = 1;
 
-                if (abs(prevdiff) <= threshold) {
+                if (abs(prevdiff) <= temporal_threshold) {
                     sum += prev;
                     count++;
                 }
 
-                if (abs(nextdiff) <= threshold) {
+                if (abs(nextdiff) <= temporal_threshold) {
                     sum += next;
                     count++;
                 }
@@ -270,7 +275,9 @@ static void fluxsmooth_temporal_uint8_c(const uint8_t *srcpp, const uint8_t *src
 }
 
 
-static void fluxsmooth_temporal_uint16_c(const uint8_t *srcpp, const uint8_t *srccp, const uint8_t *srcnp, uint8_t *dstp, int width, int height, int stride, int threshold) {
+static void fluxsmooth_temporal_uint16_c(const uint8_t *srcpp, const uint8_t *srccp, const uint8_t *srcnp, uint8_t *dstp, int width, int height, int stride, int temporal_threshold, int spatial_threshold) {
+    (void)spatial_threshold;
+
     int x, y;
     int magic_numbers[] = { 0, 262144, 131072, 87381 };
 
@@ -286,12 +293,12 @@ static void fluxsmooth_temporal_uint16_c(const uint8_t *srcpp, const uint8_t *sr
                 int sum = curr;
                 int count = 1;
 
-                if (abs(prevdiff) <= threshold) {
+                if (abs(prevdiff) <= temporal_threshold) {
                     sum += prev;
                     count++;
                 }
 
-                if (abs(nextdiff) <= threshold) {
+                if (abs(nextdiff) <= temporal_threshold) {
                     sum += next;
                     count++;
                 }
@@ -314,58 +321,6 @@ static void fluxsmooth_temporal_uint16_c(const uint8_t *srcpp, const uint8_t *sr
 static void VS_CC fluxSmoothInit(VSMap *in, VSMap *out, void **instanceData, VSNode *node, VSCore *core, const VSAPI *vsapi) {
     FluxSmoothData *d = (FluxSmoothData *)*instanceData;
     vsapi->setVideoInfo(d->vi, 1, node);
-}
-
-
-static const VSFrameRef *VS_CC fluxSmoothTGetFrame(int n, int activationReason, void **instanceData, void **frameData, VSFrameContext *frameCtx, VSCore *core, const VSAPI *vsapi) {
-    FluxSmoothData *d = (FluxSmoothData *)*instanceData;
-
-    if (activationReason == arInitial) {
-        if (n == 0 || n == d->vi->numFrames - 1) {
-            vsapi->requestFrameFilter(n, d->node, frameCtx);
-        } else {
-            vsapi->requestFrameFilter(n - 1, d->node, frameCtx);
-            vsapi->requestFrameFilter(n, d->node, frameCtx);
-            vsapi->requestFrameFilter(n + 1, d->node, frameCtx);
-        }
-    } else if (activationReason == arAllFramesReady) {
-        const VSFrameRef *srcp, *srcc, *srcn;
-        VSFrameRef *dst;
-        int plane;
-
-        if (n == 0 || n == d->vi->numFrames - 1) {
-            return vsapi->getFrameFilter(n, d->node, frameCtx);
-        }
-
-        srcp = vsapi->getFrameFilter(n - 1, d->node, frameCtx);
-        srcc = vsapi->getFrameFilter(n, d->node, frameCtx);
-        srcn = vsapi->getFrameFilter(n + 1, d->node, frameCtx);
-        const VSFrameRef *frames[] = { d->process[0] ? 0 : srcc, d->process[1] ? 0 : srcc, d->process[2] ? 0 : srcc };
-        const int planes[] = { 0, 1, 2 };
-        dst = vsapi->newVideoFrame2(d->vi->format, d->vi->width, d->vi->height, frames, planes, srcc, core);
-
-        for (plane = 0; plane < d->vi->format->numPlanes; plane++) {
-            if (d->process[plane]) {
-                const uint8_t *srcpp = vsapi->getReadPtr(srcp, plane);
-                const uint8_t *srccp = vsapi->getReadPtr(srcc, plane);
-                const uint8_t *srcnp = vsapi->getReadPtr(srcn, plane);
-                uint8_t *dstp = vsapi->getWritePtr(dst, plane);
-                int width = vsapi->getFrameWidth(srcc, plane);
-                int height = vsapi->getFrameHeight(srcc, plane);
-                int stride = vsapi->getStride(srcc, plane);
-
-                d->temporal_function(srcpp, srccp, srcnp, dstp, width, height, stride, d->temporal_threshold);
-            }
-        }
-
-        vsapi->freeFrame(srcp);
-        vsapi->freeFrame(srcc);
-        vsapi->freeFrame(srcn);
-
-        return dst;
-    }
-
-    return 0;
 }
 
 
@@ -578,7 +533,7 @@ static void fluxsmooth_spatiotemporal_uint16_c(const uint8_t *srcpp, const uint8
 }
 
 
-static const VSFrameRef *VS_CC fluxSmoothSTGetFrame(int n, int activationReason, void **instanceData, void **frameData, VSFrameContext *frameCtx, VSCore *core, const VSAPI *vsapi) {
+static const VSFrameRef *VS_CC fluxSmoothGetFrame(int n, int activationReason, void **instanceData, void **frameData, VSFrameContext *frameCtx, VSCore *core, const VSAPI *vsapi) {
     FluxSmoothData *d = (FluxSmoothData *)*instanceData;
 
     if (activationReason == arInitial) {
@@ -615,7 +570,7 @@ static const VSFrameRef *VS_CC fluxSmoothSTGetFrame(int n, int activationReason,
                 int height = vsapi->getFrameHeight(srcc, plane);
                 int stride = vsapi->getStride(srcc, plane);
 
-                d->spatiotemporal_function(srcpp, srccp, srcnp, dstp, width, height, stride, d->temporal_threshold, d->spatial_threshold);
+                d->flux_function(srcpp, srccp, srcnp, dstp, width, height, stride, d->temporal_threshold, d->spatial_threshold);
             }
         }
 
@@ -644,6 +599,10 @@ static void VS_CC fluxSmoothCreate(const VSMap *in, VSMap *out, void *userData, 
     int err;
     int i, m, n, o;
     intptr_t function = (intptr_t)userData;
+    const char *filter_name = function == SpatioTemporalFlux ? "SmoothST" : "SmoothT";
+
+#define ERROR_SIZE 128
+    char error[ERROR_SIZE] = { 0 };
 
     d.temporal_threshold = vsapi->propGetInt(in, "temporal_threshold", 0, &err);
     if (err) {
@@ -657,22 +616,26 @@ static void VS_CC fluxSmoothCreate(const VSMap *in, VSMap *out, void *userData, 
         }
 
         if (d.temporal_threshold < -1) {
-            vsapi->setError(out, "SmoothST: temporal_threshold must be -1 or greater.");
+            snprintf(error, ERROR_SIZE, "%s: temporal_threshold must be -1 or greater.", filter_name);
+            vsapi->setError(out, error);
             return;
         }
 
         if (d.spatial_threshold < -1) {
-            vsapi->setError(out, "SmoothST: spatial_threshold must be -1 or greater.");
+            snprintf(error, ERROR_SIZE, "%s: spatial_threshold must be -1 or greater.", filter_name);
+            vsapi->setError(out, error);
             return;
         }
 
         if (d.temporal_threshold == -1 && d.spatial_threshold == -1) {
-            vsapi->setError(out, "SmoothST: At least one threshold must be greater than -1.");
+            snprintf(error, ERROR_SIZE, "%s: At least one threshold must be greater than -1.", filter_name);
+            vsapi->setError(out, error);
             return;
         }
     } else {
         if (d.temporal_threshold < 0) {
-            vsapi->setError(out, "SmoothT: temporal_threshold must be 0 or greater.");
+            snprintf(error, ERROR_SIZE, "%s: temporal_threshold must be 0 or greater.", filter_name);
+            vsapi->setError(out, error);
             return;
         }
     }
@@ -681,11 +644,8 @@ static void VS_CC fluxSmoothCreate(const VSMap *in, VSMap *out, void *userData, 
     d.vi = vsapi->getVideoInfo(d.node);
 
     if (!isConstantFormat(d.vi) || d.vi->format->sampleType != stInteger || d.vi->format->bitsPerSample > 16) {
-        if (function == SpatioTemporalFlux) {
-            vsapi->setError(out, "SmoothST: Only 8..16 bit integer input with constant format and dimensions supported.");
-        } else {
-            vsapi->setError(out, "SmoothT: Only 8..16 bit integer input with constant format and dimensions supported.");
-        }
+        snprintf(error, ERROR_SIZE, "%s: Only 8..16 bit integer input with constant format and dimensions supported.", filter_name);
+        vsapi->setError(out, error);
         vsapi->freeNode(d.node);
         return;
     }
@@ -716,21 +676,16 @@ static void VS_CC fluxSmoothCreate(const VSMap *in, VSMap *out, void *userData, 
         o = vsapi->propGetInt(in, "planes", i, 0);
 
         if (o < 0 || o >= n) {
-            if (function == SpatioTemporalFlux) {
-                vsapi->setError(out, "SmoothST: Plane index out of range.");
-            } else {
-                vsapi->setError(out, "SmoothT: Plane index out of range.");
-            }
+            snprintf(error, ERROR_SIZE, "%s: Plane index out of range.", filter_name);
+            vsapi->setError(out, error);
             vsapi->freeNode(d.node);
             return;
         }
 
         if (d.process[o]) {
-            if (function == SpatioTemporalFlux) {
-                vsapi->setError(out, "SmoothST: Plane specified twice.");
-            } else {
-                vsapi->setError(out, "SmoothT: Plane specified twice.");
-            }
+            vsapi->setError(out, error);
+            snprintf(error, ERROR_SIZE, "%s: Plane specified twice.", filter_name);
+#undef ERROR_SIZE
             vsapi->freeNode(d.node);
             return;
         }
@@ -739,30 +694,32 @@ static void VS_CC fluxSmoothCreate(const VSMap *in, VSMap *out, void *userData, 
     }
 
     // Select the functions.
-    if (d.vi->format->bytesPerSample == 1) {
-#if defined(FLUXSMOOTH_X86)
-        d.temporal_function = fluxsmooth_temporal_uint8_sse2;
-#else
-        d.temporal_function = fluxsmooth_temporal_uint8_c;
-#endif
-        d.spatiotemporal_function = fluxsmooth_spatiotemporal_uint8_c;
+    if (function == SpatioTemporalFlux) {
+        if (d.vi->format->bytesPerSample == 1) {
+            d.flux_function = fluxsmooth_spatiotemporal_uint8_c;
+        } else {
+            d.flux_function = fluxsmooth_spatiotemporal_uint16_c;
+        }
     } else {
+        if (d.vi->format->bytesPerSample == 1) {
 #if defined(FLUXSMOOTH_X86)
-        d.temporal_function = fluxsmooth_temporal_uint16_sse2;
+            d.flux_function = fluxsmooth_temporal_uint8_sse2;
 #else
-        d.temporal_function = fluxsmooth_temporal_uint16_c;
+            d.flux_function = fluxsmooth_temporal_uint8_c;
 #endif
-        d.spatiotemporal_function = fluxsmooth_spatiotemporal_uint16_c;
+        } else {
+#if defined(FLUXSMOOTH_X86)
+            d.flux_function = fluxsmooth_temporal_uint16_sse2;
+#else
+            d.flux_function = fluxsmooth_temporal_uint16_c;
+#endif
+        }
     }
 
     data = malloc(sizeof(d));
     *data = d;
 
-    if (function == SpatioTemporalFlux) {
-        vsapi->createFilter(in, out, "SmoothST", fluxSmoothInit, fluxSmoothSTGetFrame, fluxSmoothFree, fmParallel, 0, data, core);
-    } else {
-        vsapi->createFilter(in, out, "SmoothT", fluxSmoothInit, fluxSmoothTGetFrame, fluxSmoothFree, fmParallel, 0, data, core);
-    }
+    vsapi->createFilter(in, out, filter_name, fluxSmoothInit, fluxSmoothGetFrame, fluxSmoothFree, fmParallel, 0, data, core);
 }
 
 
